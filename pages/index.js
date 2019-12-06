667c14bf380a3115df129acd55631d2d3a97b5f4
gabrielarck <stanislasvega@yahoo.fr>
2019-12-06 16:30:57 +0100

    call Api eventbrite

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,35 @@
 import Layout from '../components/Layout';
+import fetch from 'isomorphic-unfetch';
 
-const Home = () => (
+
+const Home = ({test}) => (
   <div>
     <Layout>
       <article>
         <h1>Bienvenue sur La Sauce Numérique !</h1>
       </article>
+      
+        {test.map (y => 
+        <div key={y.id}>
+          <a href={y.url}> clique me</a>
+          <li>{y.name.text}</li>
+        </div>
+          )
+        }
+      
     </Layout>
   </div>
 );
+
+Home.getInitialProps = async function() {
+  const res = await fetch('https://www.eventbriteapi.com/v3/users/me/events/?token=J3HV53T7XHQQQ5KWAMHT');
+  const data = await res.json();
+
+  console.log(data);
+
+  return {
+    test : data.events
+  };
+};
 
 export default Home;
