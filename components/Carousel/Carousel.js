754d97baae526450e1d6a9cca2b9858ce7e5c449
Bwouno <bwouno@icloud.com>
2019-12-10 16:30:55 +0100

    Modification  button redirection link to wikipedia yes and no

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,25 +2,16 @@ import React from 'react';
 import './Carousel.css';
 import { Carousel as ResponsiveCarousel } from 'react-responsive-carousel';
 
-// function ActionLink() {
-//   function handleClick(e) {
-//     e.preventDefault();
-//     console.log('Le lien a été cliqué.');
-//   }
-
-//   return (
-//     <a href="https://wwww.google.fr" onClick={handleClick}>
-//       Clique ici
-//     </a>
-//   );
-// }
-
-
 class Carousel extends React.Component {
 
-    handleClick = (e) => {
+    NoClick = (e) => {
       e.preventDefault();
-      location = 'http://www.google.com/';
+      location = 'https://fr.wikipedia.org/wiki/Non';
+    };
+
+    YesClick = (e) => {
+      e.preventDefault();
+      location = 'https://fr.wikipedia.org/wiki/Oui';
     };
 
   render() {
@@ -29,30 +20,30 @@ class Carousel extends React.Component {
         <div className="question">
           <img src="https://i.imgur.com/02jLusq.jpg" alt="ordi" />
           <h1>Saviez-vous que les Français passe 8h/jour devant des écrans?</h1>
-          <button className="button_question" onClick={this.handleClick}>
+          <button className="button_question" onClick={this.YesClick}>
             OUI
           </button>
-          <button className="button_question" onClick={this.handleClick}>
+          <button className="button_question" onClick={this.NoClick}>
             NON
           </button>
         </div>
         <div className="question">
           <img src="https://i.imgur.com/JfnGK0j.jpg" alt="ordi" />
           <h1>Saviez-vous que les Français passe 8h/jour devant des écrans?</h1>
-          <button className="button_question" onClick={this.handleClick}>
+          <button className="button_question" onClick={this.YesClick}>
             OUI
           </button>
-          <button className="button_question" onClick={this.handleClick}>
+          <button className="button_question" onClick={this.NoClick}>
             NON
           </button>
         </div>
         <div className="question">
           <img src="https://i.imgur.com/au1L4wj.jpg" alt="ordi" />
           <h1>Saviez-vous que les Français passe 8h/jour devant des écrans?</h1>
-          <button className="button_question" onClick={this.handleClick}>
+          <button className="button_question" onClick={this.YesClick}>
             OUI
           </button>
-          <button className="button_question" onClick={this.handleClick}>
+          <button className="button_question" onClick={this.NoClick}>
             NON
           </button>
         </div>
