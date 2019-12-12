02214bbea935b11d2b5210a00d41921a4f4b64d6
vega <stanislasvega@yahoo.fr>
2019-12-12 10:42:30 +0100

    bint package.json backend on package.json frontend

--- api/server.js
+++ api/server.js
@@ -1,32 +1,30 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const connection = require("./connection");
+const express = require('express');
+const bodyParser = require('body-parser');
+const connection = require('./connection');
+
 const app = express();
 
-const PartnersRoutes = require("./routes/partners");
+const PartnersRoutes = require('./routes/partners');
 
 app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
+  bodyParser.urlencoded({
+    extended: false,
+  }),
 );
 
 app.use(bodyParser.json());
-app.use("/partners", PartnersRoutes);
+app.use('/partners', PartnersRoutes);
 
 app.get('/', (req, res) => {
-    connection.query('SELECT * FROM partenaire', (err, results) => {
-        if (err)
-        {
-            console.log(err)
-            res.status(500).send('Erreur lors de la récupeération des données')
-        }
-        else
-        {
-            res.json(results);
-        }
-    });
-})
+  connection.query('SELECT * FROM partenaire', (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Erreur lors de la récupeération des données');
+    } else {
+      res.json(results);
+    }
+  });
+});
 const port = 5000;
 
 app.listen(port, () => console.log(`Serveur up and running on port ${port}!`));
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,36 @@
-import Layout from '../components/Layout';
 import fetch from 'isomorphic-unfetch';
+import Layout from '../components/Layout';
 
 
-const Home = ({test}) => (
+const Home = ({ test }) => (
   <div>
     <Layout>
       <article>
         <h1>Bienvenue sur La Sauce Numérique !</h1>
       </article>
-      
-        {test.map (y => 
+
+      {test.map((y) => (
         <div key={y.id}>
-            <a href={y.logo}> clique me</a>
-            <div>
-              <img src={y.logo}></img>
-            </div>
+          <a href={y.logo}> clique me</a>
+          <div>
+            <img alt="" src={y.logo} />
+          </div>
           <li>{y.name}</li>
           <li>{y.id}</li>
         </div>
-          )
-        }
-      
+      ))}
+
     </Layout>
   </div>
 );
-//https://www.eventbriteapi.com/v3/users/me/events/?token=J3HV53T7XHQQQ5KWAMHT
-Home.getInitialProps = async function() {
+// https://www.eventbriteapi.com/v3/users/me/events/?token=J3HV53T7XHQQQ5KWAMHT
+Home.getInitialProps = async function () {
   const res = await fetch('http://localhost:5000');
   const data = await res.json();
   console.log(data);
 
   return {
-    test : data
+    test: data,
   };
 };
 
