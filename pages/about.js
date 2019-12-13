38d461cdcff38e69edf6959d0028e846e49ea011
vega <stanislasvega@yahoo.fr>
2019-12-13 17:15:45 +0100

    add section about-us

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,4 @@
-import Layout from '../components/Layout';
 
-const AboutUs = () => (
-  <div>
-    <Layout>
-      <article>
-        <h1>Ceci est la page : À propos</h1>
-      </article>
-    </Layout>
-  </div>
-);
-
-export default AboutUs;
 import React from 'react';
 import Head from 'next/head';
 import Layout from '../components/Layout';
