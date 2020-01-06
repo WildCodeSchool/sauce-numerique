f595fefb14d9dfa81a0ed0d3e945dd1e9ff38a6c
Alex' Le Coq <alexandre.lcoq@gmail.com>
2020-01-06 15:04:49 +0100

    Add Partner Component on Contact Page

--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,14 @@
 import Layout from "../components/Layout"
 import ContactMail from "../components/Contact/contact-mail"
 import ContactSponsor from "../components/Contact/contact-sponso"
+import ContactPartner from "../components/Contact/contact-partner"
 
 const Contact = () => (
     <div>
       <Layout>
       <ContactMail />
       <ContactSponsor />
+      <ContactPartner />
       </Layout>
     </div>
   );
