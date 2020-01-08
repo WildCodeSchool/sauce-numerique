a1b4ba172136860e302c1505451bdd778981daef
vega <stanislasvega@yahoo.fr>
2020-01-08 10:55:33 +0100

    Events.js modify ? (if) for &&

--- components/events/Events.js
+++ components/events/Events.js
@@ -5,7 +5,7 @@ const Events = ({ events })=>(
     <div>
         {events.events.map((y) => (
             <div className={"events"}  key={y.id}>
-            {y.logo ? <img className="events--logo" src={y.logo.url} />: ""}
+            {y.logo && <img className="events--logo" src={y.logo.url} />}
                 <div className="events--text">
                     <h2> {y.name.text}</h2>
                     <p>{y.description.text}</p>
