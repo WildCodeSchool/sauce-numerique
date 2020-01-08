53901c03483602752569d745fd507c4ad05f687b
vega <stanislasvega@yahoo.fr>
2020-01-08 09:20:10 +0100

    add className for events

--- components/events/Events.js
+++ components/events/Events.js
@@ -4,11 +4,12 @@ import './Events.css';
 const Events = ({ events })=>(
     <div>
         {events.events.map((y) => (
-            <div className="events" key={y.id}>
+            <div className={"events"}  key={y.id}>
             {y.logo ? <img className="events--logo" src={y.logo.url} />: ""}
                 <div className="events--text">
                     <h2> {y.name.text}</h2>
                     <p>{y.description.text}</p>
+                    <div>{y.venue_id}</div>
                 </div>
             </div>
         ))}
