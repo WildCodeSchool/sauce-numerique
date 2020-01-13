ba885e8d8b9448d7a31139c14751469292f13199
vega <stanislasvega@yahoo.fr>
2020-01-13 16:22:46 +0100

    fix bug layout charSet utf8

--- components/Layout.js
+++ components/Layout.js
@@ -6,9 +6,8 @@ import Menu from './Navbar/Menu';
 const Layout = ({ children }) => (
   <div>
     <Head>
-      <meta charset="utf-8" />
+      <meta charSet="utf-8" />
       <title>La Sauce Numérique</title>
-      <meta charset="UTF-8" />
       <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
     </Head>
