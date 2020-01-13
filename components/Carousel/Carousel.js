5378fd6ab37af3e73d0c77b161175081408be3d8
Bwouno <bwouno@icloud.com>
2020-01-13 14:40:09 +0100

    Integration Kit Design carousel css

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -23,35 +23,28 @@ class Carousel extends React.Component {
         <img src="https://i.imgur.com/dcqrhOZ.png" alt="personnes qui utilise un telephone"  className="background" />
         </div>
         <img src="https://i.imgur.com/uIVo5Ie.png" alt="personnes qui utilise un telephone"  className="imgcarousel" />
-        <h1 className="question">Saviez-vous que les Français passe 8h/jour devant des écrans?</h1>
+        <h1 className="question">En moyenne et par jour, combien de temps passons-nous devant des écrans ?</h1>
         <div className="button">
         <button className="button_question"  onClick={this.YesClick}>
-          OUI
+          Oh bah grand max 3h...
         </button>
         <button className="button_question" onClick={this.NoClick}>
-          NON
+          8h ! Et c'est bien trop !
         </button>
         </div>
       </div>
       <div>
-        <img src="https://i.imgur.com/uIVo5Ie.png" alt="personnes qui utilise un telephone"  className="imgcarousel" />        <h1 className="question">Saviez-vous que les Français passe 8h/jour devant des écrans?</h1>
-        <div className="button">
-        <button className="button_question" onClick={this.YesClick}>
-          OUI
-        </button>
-        <button className="button_question" onClick={this.NoClick}>
-          NON
-        </button>
+        <div>
+        <img src="https://i.imgur.com/dcqrhOZ.png" alt="personnes qui utilise un telephone"  className="background" />
         </div>
-      </div>
-      <div>
-        <img src="https://i.imgur.com/uIVo5Ie.png" alt="personnes qui utilise un telephone"  className="imgcarousel" />        <h1 className="question">Saviez-vous que les Français passe 8h/jour devant des écrans?</h1>
-        <div className="button">
-        <button className="button_question" onClick={this.YesClick}>
-          OUI
-        </button>
-        <button className="button_question" onClick={this.NoClick}>
-          NON
+        <img src="https://i.imgur.com/uIVo5Ie.png" alt="personnes qui utilise un telephone"  className="imgcarousel" />
+        <h1 className="question2">Eh oui… 8h d’écrans par jour avec 7 écrans par famille en moyenne.</h1>
+        <div className='button'>
+        <div>
+          <a className="question-lien" target="_blank" href="https://www.mon-enfant-et-les-ecrans.fr/quel-temps-devant-les-ecrans-faut-il-accorder-a-votre-enfant/">Je veux voir l’étude à ce sujet</a>
+        </div>
+        <button className="button_question2"  onClick={this.YesClick}>
+          Pas étonnant…
         </button>
         </div>
       </div>
