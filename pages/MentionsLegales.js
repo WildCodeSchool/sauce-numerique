4f4631a68683fc4dcd79fe9a11fe660115bbf492
jn-html <marmainjean@gmail.com>
2020-01-13 14:28:21 +0100

    Add Mentions Légales Page

---
+++ pages/MentionsLegales.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Link from 'next/link';
+
+const MentionsLegales = () => (
+    <div>
+        <h3 className="mentions-legales-title">Mentions Légales</h3>          
+    </div>
+        );
+
+
+
+export default MentionsLegales;
