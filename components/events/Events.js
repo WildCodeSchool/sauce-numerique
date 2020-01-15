73d5a0b475f7a18f16bf67a6e2a6c7a054baaaec
Bwouno <bwouno@icloud.com>
2020-01-15 16:36:57 +0100

    [Integration] css Event

--- components/events/Events.js
+++ components/events/Events.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './Events.css';
 
 const Events = ({ events })=>(
-    <div className="events-container">
+    <div className="events-events">
         {events.events.map((y) => (
+            <div className="events-container">
             <a className="url" target="_blanck" href={y.url}>
             <div className={"events"}  key={y.id}>
-            {y.logo && <img className="events-logo" src={y.logo.original.url} />}
-            {y.is_free ? <p className="events-is_free" >GRATUIT</p> : <p className="events-is_free" >PAYANT</p>}
+            <div className="event">
+                {y.logo ? <img className="events-logo" src={y.logo.original.url} /> : <img className="events-logo" src="https://i.imgur.com/eSTiexo.jpg"/>}
+                <div className="rectangle">
+                {y.is_free ? <p className="events-is_free">GRATUIT</p> : <p className="events-is_free" >PAYANT</p>}
+                </div>                
+            </div>   
                 <div className="events-text">
                     <h2 className="events-title">
                         {y.name.text}
@@ -16,8 +21,17 @@ const Events = ({ events })=>(
                         {y.description.text}
                     </p>
                 </div>
+                <div className="date">
+                    <p className="event-date">
+                        {y.start.local}
+                    </p>
+                    <p className="event-date">
+                        {y.end.local}
+                    </p>
+                </div>                
             </div>
             </a>
+            </div>
         ))}             
     </div>
     
