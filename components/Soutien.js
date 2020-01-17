1dac4e122271325b0713616d017b1878c2695866
jn-html <marmainjean@gmail.com>
2020-01-17 09:33:48 +0100

    Add button on the Soutien component

--- components/Soutien.js
+++ components/Soutien.js
@@ -7,6 +7,7 @@ const Soutien = () => (
         <hr className="hr-soutien"/>
         <p className="p-soutien">La Sauce Numérique souhaite par ses actions sur le terrain <strong>promouvoir l’accompagnement, l’apprentissage, l’accès et l’appropriation du numérique à destination du grand public</strong> avec pas ou peu de connaissance de ce vaste univers. La Sauce Numérique est une association régie par la loi du 1er juillet 1901. Ses membres sont issus du numérique et d’autres horizons, formant un groupe riche et éclectique dont l’objectif est d’organiser des actions les plus accessibles et adaptées possibles aux problèmes liées au numérique.
         </p>
+        <button className="don-button">Faire un don</button>
 
 
             <style jsx>{`
@@ -36,7 +37,13 @@ const Soutien = () => (
                     margin-top: 2vh;
                     margin-bottom: 1vh;
                 }
-                
+
+                .don-button{
+                    margin-left: 40vw;
+                    margin-top: 4vh;
+                    padding: 10px;
+                    width: 20vw;
+                }
 
 
             `}</style>     
