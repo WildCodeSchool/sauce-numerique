1704a6dca9bbd1e204d14c6b1058f7a56ad7f168
Alex' Le Coq <alexandre.lcoq@gmail.com>
2020-01-22 16:32:25 +0100

    Correction of Contact.js + Delete Soutien.js now Support

--- pages/contact.js
+++ pages/contact.js
@@ -1,15 +1,16 @@
 import Layout from "../components/Layout";
-import Soutien from '../components/Soutien';
 import Support from '../components/Support';
 
 
 const Contact = () => (
   <div>
     <Layout>
-      <h5 className="contact-title">Contact</h5>
+      <h5 className="contact-title">
+        Contact
+      </h5>
       <hr className="hr-contact" />
       <p className="p-contact">
-La Sauce Numérique souhaite par ses actions
+        La Sauce Numérique souhaite par ses actions
         sur le terrain
         promouvoir l’accompagnement, l’apprentissage, l’accès et
         l’appropriation du numérique à destination du grand public
@@ -37,9 +38,9 @@ La Sauce Numérique souhaite par ses actions
         formant un groupe riche et éclectique dont l’objectif est d’organiser
          des actions les plus accessibles et adaptées possibles aux problèmes
          liées au numérique.
+    </p>
+    <Support/>
     </Layout>
-
-
     <style jsx>
       {`
                 .contact-title{
@@ -72,8 +73,6 @@ La Sauce Numérique souhaite par ses actions
             `}
 
     </style>
-
-
   </div>
 );
 
