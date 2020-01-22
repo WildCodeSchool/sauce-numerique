7ab3d5976005203b7cf71ab7e8365a7a5edccaff
Alex' Le Coq <alexandre.lcoq@gmail.com>
2020-01-22 17:57:42 +0100

    [Refactor] Partners Text in Partners Component

--- components/partners/Partners.js
+++ components/partners/Partners.js
@@ -15,6 +15,18 @@ function Partners({ partners }) {
   };
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
+    <div className="partner-container">
+        <h1 className="title">Nos Partenaires</h1>
+        <hr className="white-break" />
+        <p className="partners-text">
+          Privée ou Public, ils nous soutiennent dans notre action pour
+          promouvoir la culture du numérique, l’accès
+          à ce formidable outil pour toutes et tous. Vous souhaitez nous
+          accompagner dans notre action ?
+        </p>
+        <a className="partners-link" href="/contact">
+          Contactez nous dès maintenant >
+        </a>
     <Slider {...settings} autoplay>
       {partners.map((y) => (
         <div className="logo-partners" key={y.id}>
@@ -22,6 +34,122 @@ function Partners({ partners }) {
         </div>
       ))}
     </Slider>
+    <style jsx>{`
+      @media screen and (max-width: 767px) {
+        .title {
+          margin: 0 auto;
+          text-align: center;
+          font-size: 6em;
+          margin-bottom: 10px;
+          font-family: "Dosis", sans-serif;
+          font-weight: bold;
+          color: #fff;
+          text-shadow: 0 1px 2px rgba(black, 0.15);
+        }
+        
+        hr.white-break {
+          width: 3rem;
+          height: 4px;
+          background-color: #fff;
+          border-style: none;
+          border-radius: 2px;
+          margin: auto;
+        }
+
+        .partners-link {
+          color: #fff;
+          font-size: 20px;
+          margin: auto;
+          display: block;
+          text-align: -webkit-center;
+        }
+        
+        .partners-text {
+          color: #fff;
+          font-size: 2rem;
+          margin: auto;
+          padding: 5rem;
+          text-align: center
+        }
+      }
+
+      @media screen and (min-width: 768px) and (max-width: 1023px) {
+        .title {
+          margin: 0 auto;
+          text-align: center;
+          font-size: 6em;
+          margin-bottom: 10px;
+          font-family: "Dosis", sans-serif;
+          font-weight: bold;
+          color: #fff;
+          text-shadow: 0 1px 2px rgba(black, 0.15);
+        }
+        
+        hr.white-break {
+          width: 3rem;
+          height: 4px;
+          background-color: #fff;
+          border-style: none;
+          border-radius: 2px;
+          margin: auto;
+        }
+
+        .partners-link {
+          color: #fff;
+          font-size: 20px;
+          margin: auto;
+          display: block;
+          text-align: -webkit-center;
+        }
+        
+        .partners-text {
+          color: #fff;
+          font-size: 2rem;
+          margin: auto;
+          padding: 8rem 15rem 8rem 15rem;
+          text-align: center;
+        }
+      }
+
+      @media screen and (min-width: 1024px) {
+        .title {
+          margin: 0 auto;
+          text-align: center;
+          font-size: 6em;
+          margin-bottom: 10px;
+          font-family: "Dosis", sans-serif;
+          font-weight: bold;
+          color: #fff;
+          text-shadow: 0 1px 2px rgba(black, 0.15);
+        }
+        
+        hr.white-break {
+          width: 3rem;
+          height: 4px;
+          background-color: #fff;
+          border-style: none;
+          border-radius: 2px;
+          margin: auto;
+        }
+
+        .partners-link {
+          color: #fff;
+          font-size: 20px;
+          margin: auto;
+          display: block;
+          text-align: -webkit-center;
+        }
+        
+        .partners-text {
+          color: #fff;
+          font-size: 2rem;
+          margin: auto;
+          padding: 5rem;
+        }
+      }
+    `}
+    </style>
+    </div>
   );
 }
 
