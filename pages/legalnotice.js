5540ea13553238d8e29c38ce4f3723e7211eb0f5
Alex' Le Coq <alexandre.lcoq@gmail.com>
2020-02-06 15:47:14 +0100

    Add margin on legal notice

--- pages/legalnotice.js
+++ pages/legalnotice.js
@@ -5,32 +5,33 @@ import Layout from '../components/Layout';
 const MentionsLegales = () => (
   <div>
     <Layout>
-      <h3 className="mentions-legales-title">Mentions Légales</h3>
-
-      {/* Lien pour générer les Mentions-Légales: http://generateur-de-mentions-legales.com/ */}
-
-      <p className="mentions-legales-text">
-            Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua.
-            Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea
-            commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate
-            velit esse cillum dolore eu fugiat
-            nulla pariatur.
-            Excepteur sint occaecat cupidatat
-            non proident, sunt in culpa
-            qui officia deserunt mollit anim
-            id est laborum.
+      <div className="legal-notice-container">
+        <h3 className="mentions-legales-title">Mentions Légales</h3>
+        {/* Lien pour générer les Mentions-Légales: http://generateur-de-mentions-legales.com/ */}
+        <p className="mentions-legales-text">
+          Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua.
+          Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure
+          dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat
+          nulla pariatur.
+          Excepteur sint occaecat cupidatat
+          non proident, sunt in culpa
+          qui officia deserunt mollit anim
+          id est laborum.
       </p>
-
+      </div>
     </Layout>
-
     <style jsx>
       {`
+            .legal-notice-container {
+              margin-top: 8rem;
+            }
             .mentions-legales-title {
                 text-align: center;
                 margin: 40px 0 0 0;
